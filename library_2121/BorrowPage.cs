bad98cd44d6b38c3e8c11c96d945931e9343f327
TML <houjunhao33@163.com>
2023-03-01 09:07:50 +0800

    Remove old GetConnection references

--- library_2121/BorrowPage.cs
+++ library_2121/BorrowPage.cs
@@ -27,68 +27,61 @@ namespace library_2121 {
 		}
 
 		private void btnSubmit_Click(object sender, EventArgs e) {
-			var conn = Utils.GetConnection();
-			try {
-				conn.Open();
-				{
-					var sc = Utils.ExecuteScalar("SELECT COUNT(借书证号) FROM reader WHERE 借书证号=@0", inputCard.Text);
-					if ((int)sc == 0) {
-						MessageBox.Show("没有此借书证号！", "错误");
-						return;
-					}
+			{
+				var sc = Utils.ExecuteScalar("SELECT COUNT(借书证号) FROM reader WHERE 借书证号=@0", inputCard.Text);
+				if ((int)sc == 0) {
+					MessageBox.Show("没有此借书证号！", "错误");
+					return;
 				}
-				{
-					var sc = Utils.ExecuteScalar("SELECT 挂失 FROM reader WHERE 借书证号=@0", inputCard.Text);
-					if ((sc as string) == "挂失") {
-						MessageBox.Show("该卡已被挂失！", "错误");
-						return;
-					}
+			}
+			{
+				var sc = Utils.ExecuteScalar("SELECT 挂失 FROM reader WHERE 借书证号=@0", inputCard.Text);
+				if ((sc as string) == "挂失") {
+					MessageBox.Show("该卡已被挂失！", "错误");
+					return;
 				}
-				{
-					var sc = Utils.ExecuteScalar("SELECT 状态 FROM book WHERE 图书编号=@0", inputBook.Text) as string;
-					if (sc == null) {
-						MessageBox.Show("没有此图书编号！", "错误");
-						return;
-					}
-					if (sc == "借") {
-						MessageBox.Show("该图书已借出！", "错误");
-						return;
-					}
+			}
+			{
+				var sc = Utils.ExecuteScalar("SELECT 状态 FROM book WHERE 图书编号=@0", inputBook.Text) as string;
+				if (sc == null) {
+					MessageBox.Show("没有此图书编号！", "错误");
+					return;
 				}
-				{
-					try {
-						var sc = Utils.ExecuteScalar("SELECT COUNT(*) FROM yuyue book WHERE 图书编号=@0 AND 有效期限>@1 AND 借书证号=@2", inputBook.Text, DateTime.Today, inputCard.Text);
-						if ((int)sc > 0) {
-							Utils.ExecuteUpdate("DELETE FROM yuyue where 图书编号=@0 AND 有效期限>@1 AND 借书证号=@2", inputBook.Text, DateTime.Today, inputCard.Text);
-						}
-						var sqlR0 = Utils.ExecuteUpdate("UPDATE book SET 状态='借' WHERE 图书编号=@0", inputBook.Text) > 0;
-
-						var type = Utils.ExecuteScalar("SELECT 级别 FROM reader WHERE 借书证号=@0", inputCard.Text).ToString();
-						var borrowTime = DateTime.Today;
-						var returnTime = borrowTime.AddDays(type == "B" ? 180 : 90);
-						var sqlR1 = Utils.ExecuteUpdate(
-							String.Format(
-								"INSERT INTO borrow(图书编号,书名,借书证号,姓名,借书日期,状态,罚款金额,还书日期) SELECT 图书编号,书名,借书证号,姓名,'{0}','否',0,'{1}' FROM book,reader WHERE book.图书编号=@0 AND reader.借书证号=@1",
-								borrowTime, returnTime
-							),
-							inputBook.Text, inputCard.Text
-						) > 0;
-
-
-						var sqlR2 = Utils.ExecuteUpdate("UPDATE reader set 已借书数=已借书数+1 WHERE 借书证号=@0", inputCard.Text) > 0;
-
-						if (!sqlR0 || !sqlR1 || !sqlR2) {
-							throw new Exception();
-						}
-
-						MessageBox.Show("借书成功！");
-					} catch {
-						MessageBox.Show("借书失败！");
-					}
+				if (sc == "借") {
+					MessageBox.Show("该图书已借出！", "错误");
+					return;
 				}
+			}
+			{
+				try {
+					var sc = Utils.ExecuteScalar("SELECT COUNT(*) FROM yuyue book WHERE 图书编号=@0 AND 有效期限>@1 AND 借书证号=@2", inputBook.Text, DateTime.Today, inputCard.Text);
+					if ((int)sc > 0) {
+						Utils.ExecuteUpdate("DELETE FROM yuyue where 图书编号=@0 AND 有效期限>@1 AND 借书证号=@2", inputBook.Text, DateTime.Today, inputCard.Text);
+					}
+					var sqlR0 = Utils.ExecuteUpdate("UPDATE book SET 状态='借' WHERE 图书编号=@0", inputBook.Text) > 0;
 
-			} finally {
-				conn?.Close();
+					var type = Utils.ExecuteScalar("SELECT 级别 FROM reader WHERE 借书证号=@0", inputCard.Text).ToString();
+					var borrowTime = DateTime.Today;
+					var returnTime = borrowTime.AddDays(type == "B" ? 180 : 90);
+					var sqlR1 = Utils.ExecuteUpdate(
+						String.Format(
+							"INSERT INTO borrow(图书编号,书名,借书证号,姓名,借书日期,状态,罚款金额,还书日期) SELECT 图书编号,书名,借书证号,姓名,'{0}','否',0,'{1}' FROM book,reader WHERE book.图书编号=@0 AND reader.借书证号=@1",
+							borrowTime, returnTime
+						),
+						inputBook.Text, inputCard.Text
+					) > 0;
+
+
+					var sqlR2 = Utils.ExecuteUpdate("UPDATE reader set 已借书数=已借书数+1 WHERE 借书证号=@0", inputCard.Text) > 0;
+
+					if (!sqlR0 || !sqlR1 || !sqlR2) {
+						throw new Exception();
+					}
+
+					MessageBox.Show("借书成功！");
+				} catch {
+					MessageBox.Show("借书失败！");
+				}
 			}
 			RefreshData(inputCard.Text);
 		}
